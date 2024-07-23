dc8c51199035276d82a44f6bcd104baa94725fcc
howabsurd <poptanikrish1@gmail.com>
2024-07-23 19:20:54 +0530

    Find Pair Given Difference

---
+++ Day 7/Find Pair Given Difference.cpp
@@ -0,0 +1,8 @@
+   int findPair(int n, int x, vector<int> &nums) {
+        unordered_map<int,int> mp;
+        for(int i=0;i<n;i++){
+            if(mp.find(nums[i]-x)!=mp.end() || mp.find(nums[i]+x)!=mp.end()) return true;
+            mp[nums[i]]++;
+        }
+        return -1;
+    }
